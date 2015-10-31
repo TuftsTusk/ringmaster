1ac78e4b50aa1aaabcf8ba7f70a82bc7debd6428
Michael Seltzer <michael.m.seltzer@gmail.com>
2015-10-31 15:16:57 -0400

    add integration route test

---
+++ test/routes.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var request = require('supertest');
+//sample test
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+describe('GET /getListings', function(){
+  it('should respond with json', function(done){
+    request('http://localhost:8080')
+      .get('/getListings')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+});
