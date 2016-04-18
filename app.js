a29d451b0e9881a447d982453e3c8451f698a6ee
BCefali <brian.cefali@gmail.com>
2016-04-18 17:14:50 -0400

    Fixed some bugs with login sessions and returning listings when logged in

--- app.js
+++ app.js
@@ -124,8 +124,6 @@ app.put('/listing/:listing_id/:listing_attr', ListingRoutes.putListingAttributeB
 app.route('/listing')
     .post(ListingRoutes.postListing)
     .get(ListingRoutes.getListing);
-app.route('/listing/:uid')
-        .get(ListingRoutes.getListingById);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
