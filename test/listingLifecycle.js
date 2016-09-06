b65e85cf62a2bbeb0fa2a825fdc03f2d5c72ad74
BCefali <brian.cefali@gmail.com>
2016-09-05 23:17:33 -0400

    POST listing by id bugfix with test

--- test/listingLifecycle.js
+++ test/listingLifecycle.js
@@ -39,6 +39,15 @@ var getMiscPostById = function(cookie, id, callback) {
         .end(callback);
 }
 
+var postMiscPostById = function(cookie, id, data, callback) {
+    request(app)
+        .post('/listing/'+id)
+        .send(data)
+        .set('Cookie', cookie)
+        .set('Accept', 'application/json')
+        .end(callback);
+}
+
 var deletePostFromId = function(id, callback) {
     m_listings.model.findOneAndRemove({_id:id}, function(err, listing) {
         var _status = (!err && listing) ? 204 : 500;
@@ -126,6 +135,45 @@ describe('Listing lifecycle', function() {
                 getMiscPostById(cookie, rsc_id, function(err, res) {
                     if (err) done(err);
                     expect(res.status).to.equal(200);
+                    deletePostFromId(rsc_id, function(err, res) {
+                        SessionStorage.destroy(sid);
+                        purgeUsers();
+                        done();
+                    });
+                })
+            });
+        });
+    });
+
+    it('Successfully add, edit, and remove a single listing', function(done) {
+        var email = 'some.jerk@tufts.edu';
+        var pass = 'foo';
+        account.logInToAccount(email, pass, function(err, res) {
+            if (err) done(err);
+            expect(res.status).to.equal(204);
+            var cookie = res.headers['set-cookie'][0];
+            var re_sid = /sid=s%3A([^;\.]+)/;
+            var sid = re_sid.exec(cookie)[1];
+            makeMiscPost(cookie, {
+                type: 'MiscListing',
+                title: 'Hurr Durr',
+                description: 'Something for sale!@!!!!0!',
+                price: 100
+            }, function(err, res) {
+                if (err) done(err);
+                expect(res.status).to.equal(201);
+                var rsc_id = res.body.rsc_id;
+                var data = {
+                    title: 'New Title',
+                    description: 'descriptiooooooooon hereeeeeeeee',
+                    price: 1,
+                    integrity: {
+                        visible: false
+                    }
+                };
+                postMiscPostById(cookie, rsc_id, data, function(err, res) {
+                    if (err) done(err);
+                    expect(res.status).to.equal(204);
                     deletePostFromId(rsc_id, function(err, res) {
                         SessionStorage.destroy(sid);
                         purgeUsers();
